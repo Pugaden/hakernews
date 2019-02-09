bbae4aec7e4b33cf94d5ce337d99aa4ad9a510e5
Den_21 <pugachev.denis1@gmail.com>
2019-02-09 14:02:32 +0200

    display list of items(map)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/rect/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = "- welcome to React...";
-    const user = {
-      name:"Den",
-      secondName:"Pugachov",
-    };
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>{user.name} {user.secondName} {helloWorld}</h2>
-        </header>
+        { list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.authour}, </span>
+              <span>comments {item.num_comments}, </span>
+              <span>points {item.points};</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
