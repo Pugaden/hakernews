73af9210e86443a631f72d7b9b76490efed62fe7
Den_21 <pugachev.denis1@gmail.com>
2019-02-17 20:54:11 +0200

    page 32 "this.state"

--- src/App.js
+++ src/App.js
@@ -21,24 +21,32 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    }; 
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(function(item) {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.authour}, </span>
-              <span>comments {item.num_comments}, </span>
-              <span>points {item.points};</span>
-            </div>
-          );
-        })}
+        { this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.authour}, </span>
+            <span>comments {item.num_comments}, </span>
+            <span>points {item.points};</span>
+          </div>
+        )}
       </div>
     );
   }
 }
+
 
 export default App;
