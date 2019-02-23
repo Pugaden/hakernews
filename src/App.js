74d5d63edbe34958fffadd24f6a1d11b92f48d08
Den_21 <pugachev.denis1@gmail.com>
2019-02-23 22:11:52 +0200

    split up components

--- src/App.js
+++ src/App.js
@@ -67,28 +67,57 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state; //ES6 Destucturing;
     return (
       <div className="App">
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange} 
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
         <form>
           <input
             type="text"
-            onChange={this.onSearchChange}
-          />
+            value={value}
+            onChange={onChange}
+            />
         </form>
-        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+      );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.authour}, </span>
-            <span>comments {item.num_comments}, </span>
-            <span>points {item.points};</span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
             <span>
               <button
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
-                Dismiss 
+                Dismiss
               </button>
             </span>
           </div>
@@ -97,6 +126,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
